c1431b2da75fe8d02b0435a3d6b5745f655668ef
AusraSima <ausrsima@yahoo.com>
2026-05-14 19:10:02 +0300

    User repository created

---
+++ BackEnd/KuroOpti/KuroOpti.Repositories/Implementations/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using KuroOpti.Data;
+using KuroOpti.Entities;
+using KuroOpti.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace KuroOpti.Repositories.Implementations
+{
+	public class UserRepository : IUserRepository
+	{
+		private readonly KuroOptiDbContext _context;
+
+		public UserRepository(KuroOptiDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<int> CreateAsync(User user)
+		{
+			_context.Users.Add(user);
+			await _context.SaveChangesAsync();
+			return user.Id;
+		}
+
+		public async Task<User> GetByIdAsync(int id)
+		{
+			return await _context.Users.FindAsync(id);
+		}
+
+		public async Task UpdateAsync(User user)
+		{
+			_context.Users.Update(user);
+			await _context.SaveChangesAsync();
+		}
+
+		public async Task DeleteAsync(int id)
+		{
+			await _context.Users.Where(u => u.Id == id).ExecuteDeleteAsync();
+			await _context.SaveChangesAsync();
+		}
+
+		public async Task<List<User>> GetAllAsync(int page, int itemsPerPage)
+		{
+			return await _context.Users.Skip((page - 1) * itemsPerPage).Take(itemsPerPage).ToListAsync();
+		}
+	}
+}
---
+++ BackEnd/KuroOpti/KuroOpti.Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using KuroOpti.Entities;
+
+namespace KuroOpti.Repositories.Interfaces
+{
+	public interface IUserRepository
+	{
+		Task<int> CreateAsync(User user);
+		Task<User> GetByIdAsync(int id);
+		Task UpdateAsync(User user);
+		Task DeleteAsync(int id);
+		Task<List<User>> GetAllAsync(int page, int itemsPerPage);
+
+	}
+}
