38ca843de7471a26d5a54d56a693afd46536d6af
unknown <simatolskisnikas@gmail.com>
2026-05-19 22:44:51 +0300

    start-task

--- BackEnd/KuroOpti/KuroOpti.Services/Implementations/FuelStationService.cs
+++ BackEnd/KuroOpti/KuroOpti.Services/Implementations/FuelStationService.cs
@@ -5,28 +5,5 @@ namespace KuroOpti.Services;
 
 public class FuelStationService : IFuelStationService
 {
-    public Task<List<FuelStation>> GetAllFuelStations()
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<FuelStation> GetFuelStationById(int id)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<FuelStation> CreateFuelStation(FuelStation fuelStation)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<FuelStation> UpdateFuelStation(int id, FuelStation fuelStation)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<bool> DeleteFuelStation(int id)
-    {
-        throw new NotImplementedException();
-    }
+    
 }
--- BackEnd/KuroOpti/KuroOpti.Services/Interfaces/IFuelStationService.cs
+++ BackEnd/KuroOpti/KuroOpti.Services/Interfaces/IFuelStationService.cs
@@ -1,12 +1,71 @@
 using KuroOpti.Entities;
-namespace KuroOpti.Services.Interfaces
+using KuroOpti.Repositories;
+using KuroOpti.Services.Interfaces;
+
+namespace KuroOpti.Services
 {
-    public interface IFuelStationService
+    public class FuelStationService : IFuelStationService
     {
-        Task<List<FuelStation>> GetAllFuelStations();
-        Task<FuelStation> GetFuelStationById(int id);
-        Task<FuelStation> CreateFuelStation(FuelStation fuelStation);
-        Task<FuelStation> UpdateFuelStation(int id, FuelStation fuelStation);
-        Task<bool> DeleteFuelStation(int id);
+        private readonly IFuelStationRepository _repository;
+
+        public FuelStationService(IFuelStationRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<FuelStation>> GetAllFuelStations()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<FuelStation> GetFuelStationById(int id)
+        {
+            var station = await _repository.GetByIdAsync(id);
+
+            if (station == null)
+            {
+                throw new Exception("Fuel station not found");
+            }
+
+            return station;
+        }
+
+        public async Task<FuelStation> CreateFuelStation(FuelStation fuelStation)
+        {
+            await _repository.AddAsync(fuelStation);
+
+            return fuelStation;
+        }
+
+        public async Task<FuelStation> UpdateFuelStation(int id, FuelStation fuelStation)
+        {
+            var existingStation = await _repository.GetByIdAsync(id);
+
+            if (existingStation == null)
+            {
+                throw new Exception("Fuel station not found");
+            }
+
+            existingStation.Name = fuelStation.Name;
+            existingStation.Address = fuelStation.Address;
+
+            await _repository.UpdateAsync(existingStation);
+
+            return existingStation;
+        }
+
+        public async Task<bool> DeleteFuelStation(int id)
+        {
+            var station = await _repository.GetByIdAsync(id);
+
+            if (station == null)
+            {
+                return false;
+            }
+
+            await _repository.DeleteAsync(id);
+
+            return true;
+        }
     }
 }
