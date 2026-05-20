a17f494d9aacbe6ac5dee6ae949d0f0ed841a967
Lukas <lukasberzis2@gmail.com>
2026-05-20 16:27:08 +0300

    Finished creating fuel station controller
    Sukuriau fuel staion controller , bet gaunu missing reffrence klaidas nes tie failai yra kitame branch.

---
+++ BackEnd/KuroOpti.API/Controllers/FuelStationController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using KuroOpti.Services.Interfaces;
+using KuroOpti.Entities;
+
+namespace KuroOpti.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FuelStationController : ControllerBase
+    {
+        private readonly IFuelStationService _fuelStationService;
+
+        public FuelStationController(IFuelStationService fuelStationService)
+        {
+            _fuelStationService = fuelStationService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllFuelStations()
+        {
+            var stations = await _fuelStationService.GetAllFuelStations();
+
+            return Ok(stations);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFuelStationsById(int id)
+        {
+            try
+            {
+                var station = await _fuelStationService.GetFuelStationById(id);
+
+                return Ok(station);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFuelStation(FuelStation fuelStation)
+        {
+            var createdStation = await _fuelStationService.CreateFuelStation(fuelStation);
+
+            return Ok(createdStation);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateFuelStation(int id, FuelStation fuelStation)
+        {
+            try
+            {
+                var updateStation = await _fuelStationService.UpdateFuelStation(id, fuelStation);
+
+                return Ok(updateStation);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFuelStation(int id)
+        {
+            var deleted = await _fuelStationService.DeleteFuelStation(id);
+
+            if (!deleted)
+            {
+                return NotFound("Fuel station not found");
+            }
+
+            return Ok("Fuel station deleted");
+        }
+    }
+}
